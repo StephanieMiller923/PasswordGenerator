0e50cc6d2e077fbc23d579c4adb32b09f8f1551f
Stephanie Miller <stephanie.miller923@gmail.com>
2021-03-22 13:49:01 -0500

    Selected necessaryDOM elements. Also, created the generatePassword function.

--- app.js
+++ app.js
@@ -41,5 +41,89 @@ function getRandomSymbol(){
 }
 console.log(getRandomSymbol());
 
+// Object to store all the generator functions
+const randomFunc = {
+    upper: getRandomUpper,
+    lower: getRandomLower,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
+
+// Selecting the DOM elements
+const resultEL = document.querySelector(`#result`);
+const lengthEL = document.querySelector(`#length`);
+const uppercaseEl = document.querySelector(`#uppercase`);
+const lowercaseEl = document.querySelector(`#lowercase`);
+const numbersEl = document.querySelector(`#numbers`);
+const symbolsEl = document.querySelector(`#symbols`);
+const generateEl = document.querySelector(`#generate`);
+const clipboardEl = document.querySelector(`#clipboard`);
+
+// Generate Password Function (Function that accepts true or false values  as well as a number as arguments.)
+function generatePassword(upper, lower, number, symbol, length){
+    // console.log(upper, lower, number, symbol, length);
+
+    // 1. Create the password variable
+    let generatedPassword = ``;
+
+    // 2. Filter out unchecked types
+    // True and False values can be added together (True is 1 and False is 0)
+    const typesCount = upper + lower + number + symbol;
+    console.log(typesCount);
+
+    // If user has not selected any of the four options, then display alert and return an empty string from the function
+    if (typesCount === 0){
+        alert(`Please select at least one option`);
+        return "";
+    }
+
+    let typesArr = [
+        [`upper`, upper],
+        [`lower`, lower],
+        [`number`, number],
+        [`symbol`, symbol]
+    ];
+
+    // The filter method creates a new array with all the elements that pass the test implemented by the provided function.
+    typesArr.filter(item => {
+        console.log(item[1]);
+        return item[1];
+    });
+    console.log(typesArr);
+
+    // 3. Loop over the length and call the generator function for each checked type
+    // Building password with a for loop!
+    for (i = 0; i < length; i += typesCount){
+        typesArr.forEach(type => {
+            const funcName = type[0];
+            console.log(funcName);
+            generatedPassword += randomFunc[funcName]();
+            console.log(generatedPassword);
+        });
+    }
+
+    // 4. Add the final passwored to the password variable and return it from the function
+    // Removing extra characters if necessary ()
+    const finalPassword = generatedPassword.slice(0, length);
+
+    return finalPassword;
+}
+
+// generatePassword(true, false, true, false, 10);
+
+// Event listener for when the "Generate Password" button is clicked
+generateEl.addEventListener(`click`, () => {
+    // Changing the value from a string to a number
+    const length = parseInt(lengthEl.value);
+
+    // Checking if the following options/checkboxes are selected/checked, and setting the returned true/false values to the respective variables!
+    const hasUpper = uppercaseEl.checked;
+    const hasLower = lowercaseEl.checked;
+    const hasNumber = numbersEl.checked;
+    const hasSymbol = symbolsEl.checked;
+
+    console.log(hasUpper, hasLower, hasNumber, hasSymbol, length);
+    
+});
 
 
